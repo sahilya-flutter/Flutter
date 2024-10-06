9cbee21b722c86ed3ca23cd84d35265171d67fb2
sahilya-flutter <sahilflutter3232@gmail.com>
2024-10-06 18:03:45 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:assigment2/demo.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ShoppingCartScreen(),
+    );
+  }
+}
